84bd8c300f7b14e19c0849611975749e624d1832
renanmaringolo <renan.maringolo@gmail.com>
2018-03-19 17:33:02 -0300

    send readme

--- README.md
+++ README.md
@@ -8,7 +8,18 @@
 ## Installation
 
   - npm install
+  - ensure MongoDB is running at the 27017 port
 
 ## Development server
 
   - node server.js
+  - Run node server.js for a dev server. The API will listen to http://localhost:8080
+
+
+## API
+
+- GET /api/heroes
+- POST /api/heroes
+- GET /api/heroes/:id
+- PUT /api/heroes/:id
+- DELETE /api/heroes/:id
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var router = express.Router();
 router.use(function(req, res, next) {
     console.log('What did u do?');
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -49,7 +50,7 @@ router.route('/heroes')
 router.route('/heroes/:hero_id')
 
     .get(function(req, res) {
-        Hero.findById(req.params.bear_id, function(err, hero) {
+        Hero.findById(req.params.hero_id, function(err, hero) {
             if (err)
                 res.send(err);
             res.json(hero);
